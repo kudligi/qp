fa783cfd2a98d41e75202191fa58536e4b0b8862
kudligi <nihal1011ml@gmail.com>
2019-03-26 10:46:59 +0530

    cleaned up csv files

---
+++ source/metadata.py
@@ -0,0 +1,31 @@
+import os
+
+
+#print(courses)
+
+def get_course_list():
+    courses = os.listdir('../data')
+    return courses
+
+def get_departments_list(course):
+    courses = get_course_list()
+    if course not in courses:
+        return "Invalid course input"
+    departments = os.listdir('../data/' + course)
+    return departments
+
+def get_papers_list(course, department):
+    departments = get_departments_list(course)
+    if departments == "Invalid course input":
+        return "Invalid course input"
+    if department not in departments:
+        return "Invalid department input"
+
+    papar_list = os.listdir('../data/' + course + '/' + department)
+    return papar_list
+
+
+#print(get_course_list())
+#print(get_departments_list('apple'))
+#print(get_departments_list('UG'))
+print(get_papers_list('UG','community_medicine_1'))
